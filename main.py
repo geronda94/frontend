59bd81080c719eac5281c802c3e431b563b9c138
Igor Ubuntu <iiiliev.igor@gmail.com>
2023-03-13 22:39:51 +0200

    qwertyu

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('hi')
+from flask import render_template, Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
