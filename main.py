7c9af23327798b31cf2c7a865084371478d0702b
Igor <Iiiliev.igor@gmail.com>
2023-03-14 14:04:17 +0200

    qwertyui

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, url_for
 
+
+
+menu_dict = [
+    {'title':'Главная','link':'index'},
+    {'title':'О нас','link':'about'}
+]
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='HTML')
+    return render_template('index.html', title='HTML', menu=menu_dict)
 
 
 
-
+@app.route('/about')
+def about():
+    return render_template('index.html', title='About us', menu=menu_dict)
 
 
 
