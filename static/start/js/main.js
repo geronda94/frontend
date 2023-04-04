e05978fd5330d947729a3ca8eeb37b5de4a2070d
Igor <Iiiliev.igor@gmail.com>
2023-04-04 14:23:50 +0300

    1234567

--- static/start/js/main.js
+++ static/start/js/main.js
@@ -1,9 +1,3 @@
-$(function() {
-    $('.bike-slider, .slider__items').slick({
-            arrows: false,
-            dots: true,
-            fade: true,
-            autoplay: true,
-            autoplaySpedd: 500
-    });
+$(function(){
+
 });
